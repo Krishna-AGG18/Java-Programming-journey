9a5b27a635aa04d78919547db10f64314b932475
KRISHNA <kraggr2909@gmail.com>
2025-01-08 17:02:42 +0530

    up

--- firstClass.java
+++ firstClass.java
@@ -1,6 +1,21 @@
+
 //First Java Program
-class firstClass{
+import java.util.*;
+
+class firstClass {
     public static void main(String[] args) {
-        System.out.println("Hello World"); //single quote is permitted
+        System.out.println("Hello World"); // single quote is permitted ,print and move to next line
+        System.out.print("hello world"); // print and remainn in same line as like c
+
+        // variables
+        int a = 10;
+        String name = "Krishna";
+        double price = 25.50;
+
+        // input create scannr obj then use its functions :-)
+        Scanner sc = new Scanner(System.in);
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
     }
 }
